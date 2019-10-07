6b8ff7d91ce53ffe65ad245a91a495f9408d0de7
Ray Hidayat <ray.hidayat@gmail.com>
2019-10-08 00:52:56 +1300

    Deduplication

--- src/agent/Actor.ts
+++ src/agent/Actor.ts
@@ -19,25 +19,60 @@ export default class Actor {
     choose(): Map<number, w.Action> {
         const robots = this.world.entities.filter(r => r.type === w.ItemType.RobotTeam0);
 
-        const actions = new Map<number, w.Action>();
+        const potentialActions = this.evaluateChoices(robots);
+
+        let deduplicationAttempts = 0;
+        for (let attempt = 0; attempt < this.world.numRobots; ++attempt) {
+            const changed = this.subsumeActions(robots, potentialActions);
+            if (!changed) {
+                break;
+            }
+
+            ++deduplicationAttempts;
+        }
+        console.error(`Deduplications: ${deduplicationAttempts}`);
+
+        const result = new Map<number, w.Action>();
+        for (const robot of robots) {
+            const potentials = potentialActions.get(robot.id);
+            const actionValue = potentials[0] || this.generateNoop(robot.id);
+            result.set(robot.id, actionValue.action);
+        }
+        return result;
+    }
+
+    private subsumeActions(robots: w.Entity[], potentialActions: Map<number, ActionValue[]>): boolean {
+        let changed = false;
+        for (const robot1 of robots) {
+            const potentials1 = potentialActions.get(robot1.id);
+            const action1 = potentials1[0];
+            for (const robot2 of robots) {
+                const potentials2 = potentialActions.get(robot2.id);
+                const action2 = potentials2[0];
+
+                if (action1 && action2 && action1.subsumes(action2)) {
+                    potentials2.shift(); // Give the action to robot 1, give robot 2 something else to do
+                    changed = true;
+                }
+            }
+        }
+        return changed;
+    }
+
+    private evaluateChoices(robots: w.Entity[]): Map<number, ActionValue[]> {
+        const result = new Map<number, ActionValue[]>();
         for (const robot of robots) {
             const actionValues = this.evaluateRobotChoices(robot);
-            const action = actionValues[0].action;
-            actions.set(robot.id, action);
+            result.set(robot.id, actionValues);
         }
-
-        // TODO: Deduplicate actions
-        return actions;
+        return result;
     }
 
     // Return actions, sorted best to worst
     private evaluateRobotChoices(robot: w.Entity): ActionValue[] {
         const actions = new Array<ActionValue>();
         if (robot.dead) {
-            actions.push(new ActionValue(0, {
-                entityId: robot.id,
-                type: "wait",
-            }));
+            actions.push(this.generateNoop(robot.id));
         } else {
             const pathMap = PathMap.generate(robot.pos, this.world, this.explosionMap);
             if (robot.carrying === w.ItemType.Ore && robot.pos.x > 0) {
@@ -64,5 +99,12 @@ export default class Actor {
             }
         }
         return actions;
+    }
+
+    private generateNoop(robotId: number): ActionValue {
+        return new ActionValue(0, {
+            entityId: robotId,
+            type: "wait",
+        });
     }
 }
